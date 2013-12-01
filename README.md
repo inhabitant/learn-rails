9db073ff8523028bffc375d7dbfd4a46e96d0d72
Juan Andres <juanandrespeiro@gmail.com>
2013-12-01 13:09:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+== README
+
+This is my first Rails app. 
+I’m using Rails 4.0.1 and the Learn Rails book from the RailsApp Project.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRails::Application.config.secret_key_base = 'ff1b2531692d3de5a04c950630b6597665466c42361a387aa579aaec69d2e21cd0c2b8fc6eb429f20dce167869398fd52b10415c34c0f8a8d3db6c2781ecac60'
